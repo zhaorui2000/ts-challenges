c0d66db96ef6f3a9b3072a82e5435de68cf88950
赵睿 <zhaorui27@sfmail.sf-express.com>
2022-04-08 14:45:12 +0800

    feat: 3312-easy-parameters

---
+++ src/3312-easy-parameters/test-cases.ts
@@ -0,0 +1,14 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+
+// @ts-ignore
+const foo = (arg1: string, arg2: number): void => {}
+// @ts-ignore
+const bar = (arg1: boolean, arg2: {a: 'A'}): void => {}
+const baz = (): void => {}
+
+type cases = [
+  Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
+  Expect<Equal<MyParameters<typeof bar>, [boolean, {a: 'A'}]>>,
+  Expect<Equal<MyParameters<typeof baz>, []>>,
+]
